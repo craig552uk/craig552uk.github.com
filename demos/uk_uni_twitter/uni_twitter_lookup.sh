1dd4274073edefbc226e78d02ecf27304dfe8b78
Craig Russell <craig@craig-russell.co.uk>
2012-05-22 21:44:13 +0100

    Updated account info. Script now supports >100 accounts

--- demos/uk_uni_twitter/uni_twitter_lookup.sh
+++ demos/uk_uni_twitter/uni_twitter_lookup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-SCREEN_NAME='Cambridge_Uni,uniofoxford,univofstandrews,uclnews,warwickuni,LancasterUni,durham_uni,lborouniversity,imperialcollege,uniofexeter,SOASFeed,UniOfBath,uniofleicester,uniofeastanglia,UniofNottingham,uniofsurrey,GlasgowUni,CityUniLondon,unikent,HeriotWattUni,unisouthampton,unistrathclyde,kingscollegelon,NewUniPress,unirdg_news,sheffielduni,astonuniversity,QMUL,universityleeds,RobertGordonUni,Uni_of_Essex,AdmissionsUoM,bournemouthuni,oxford_brookes,StirUni,ucfalmouth,AberUni,queensubelfast,EdinburghNapier,HuddersfieldUni,KeeleUniversity,QMUniversity,unilincoln,chiuni,uwebristol,TeessideUni,uniofglos,GoldsmithsUOL,covcampus,UniNorthants,BathSpaUni,trentuni,uniglam,ucfalmouth,cardiffmet,uniofbrighton,CaledonianNews,StaffsUni,edgehill,BangorUni,uochester,westlondonguru,bruneluni,YourStMarys,LeedsTrinity,portsmouthuni,BradfordUni,UniWestminster,CumbriaUni,MyBCU,angliaruskin,dmuleicester,SwanseaUni,Yorkstjohn,newman_uni,unigreenwich,lsbu,trinitystdavid,uniofbeds,glyndwruni,leedsmet,worcester_uni,AbertayUni,canterburyccuni,ucpmarjon,SalfordUni,LJMU,bucksnewuni,solentofficial,newportuni,uel_news,BoltonUni,UCS_NEWS,UniWestScotland,londonmetuni'
+# Batches of max 100
+SCREEN_NAME_1='aberdeenuni,AbertayUni,AberUni,angliaruskin,astonuniversity,BangorUni,UniOfBath,BathSpaUni,uniofbeds,MyBCU,BoltonUni,bournemouthuni,BradfordUni,uniofbrighton,bruneluni,bucksnewuni,Cambridge_Uni,canterburyccuni,uochester,chiuni,CityUniLondon,covcampus,CumbriaUni,dmuleicester,durham_uni,uel_news,edgehill,UniofEdinburgh,EdinburghNapier,Uni_of_Essex,uniofexeter,ucfalmouth,uniglam,GlasgowUni,CaledonianNews,uniofglos,glyndwruni,GoldsmithsUOL,unigreenwich,HeriotWattUni,HuddersfieldUni,UniOfHull,imperialcollege,KeeleUniversity,unikent,kingscollegelon,LancasterUni,universityleeds,leedsmet,LeedsTrinity,uniofleicester,unilincoln,LJMU,londonmetuni,lsenews,lsbu,lborouniversity,AdmissionsUoM,manmetuni,ucpmarjon,NewUniPress,newman_uni,newportuni,UniNorthants,UniofNottingham,trentuni,openuniversity,uniofoxford,oxford_brookes,plymuni,portsmouthuni,QMUniversity,QMUL,queensubelfast,unirdg_news,RobertGordonUni,SalfordUni,sheffielduni,SOASFeed,unisouthampton,solentofficial,univofstandrews,YourStMarys,StaffsUni,StirUni,unistrathclyde,uniofsurrey,SwanseaUni,TeessideUni,trinitystdavid,ucfalmouth,UCS_NEWS,uclnews,uniofeastanglia,ulsteruni,uwebristol,cardiffmet,warwickuni,westlondonguru'
 
-URL='http://api.twitter.com/1/users/lookup.json?screen_name='$SCREEN_NAME
+SCREEN_NAME_2='UniWestScotland,UniWestminster,worcester_uni,uniofyork,Yorkstjohn'
 
-curl $URL > 'uk_uni_twitter.json'
+URL_HEAD='http://api.twitter.com/1/users/lookup.json?screen_name='
+
+curl $URL_HEAD$SCREEN_NAME_1 >  'uk_uni_twitter.tmp'
+curl $URL_HEAD$SCREEN_NAME_2 >> 'uk_uni_twitter.tmp'
+
+cat 'uk_uni_twitter.tmp' | sed 's/\]\[/,/g' > 'uk_uni_twitter.json'
+
+rm 'uk_uni_twitter.tmp'
